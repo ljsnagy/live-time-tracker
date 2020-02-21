e4414d76dd7919938261f35c4fe94c7f95183e21
Luke Nagy <ljsn@protonmail.com>
2020-02-21 12:21:06 +0800

    fix(parseTimeSheet): Use a better supported File.arrayBuffer() alternative.

--- src/utils/parseTimeSheet.js
+++ src/utils/parseTimeSheet.js
@@ -10,20 +10,36 @@ function toMinutes(time) {
   return (Number(hour) * 60) + Number(mins);
 }
 
-export default async function parseTimeSheet(file) {
-  const data = new Uint8Array(await file.arrayBuffer());
-  const workbook = XLSX.read(data, { type: 'array' });
-  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+export default function parseTimeSheet(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
 
-  const activities = [];
+    reader.onload = (e) => {
+      try {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-  for (let row = TS_ACTIVITY_LIST_OFFSET; worksheet[`${TS_TIME_COL}${row}`]; row += 1) {
-    activities.push({
-      time: worksheet[`${TS_TIME_COL}${row}`].w,
-      name: worksheet[`${TS_ACTIVITY_COL}${row}`].w,
-      minutes: toMinutes(worksheet[`${TS_LENGTH_COL}${row}`].w),
-    });
-  }
+        const activities = [];
 
-  return activities;
+        for (
+          let row = TS_ACTIVITY_LIST_OFFSET;
+          worksheet[`${TS_TIME_COL}${row}`];
+          row += 1
+        ) {
+          activities.push({
+            time: worksheet[`${TS_TIME_COL}${row}`].w,
+            name: worksheet[`${TS_ACTIVITY_COL}${row}`].w,
+            minutes: toMinutes(worksheet[`${TS_LENGTH_COL}${row}`].w),
+          });
+        }
+
+        resolve(activities);
+      } catch (err) {
+        reject(err);
+      }
+    };
+
+    reader.readAsArrayBuffer(file);
+  });
 }
